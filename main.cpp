e21e67d2a51a908fd482ca99af97410d88d72ebd
UIT.23730220 <23730220@ms.uit.edu.vn>
2024-09-10 16:13:06 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void menuGame();
+
 int main() {
     float a,b,delta,x1,x2;
 
@@ -13,3 +15,20 @@ int main() {
 
     return 0;
 }
+
+void menuGame(){
+    cout << "Chon chuc nang duoc hien thi ben duoi:\n";
+    cout << "1. Play!\n";
+    cout << "2. Exit.\n";
+    int choice;
+    cin >> choice;
+    switch (choice){
+        case 1: 
+            break;
+        case 2:
+            break;
+        default:
+            cout << "invalid choice.\n" << endl;
+            break;
+    }
+};
