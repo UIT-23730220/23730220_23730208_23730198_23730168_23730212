43f9fbda9a01e36edbed4912108f4127dc81536f
UIT.23730168 <Phannhathoa13@gmail.com>
2024-09-11 05:39:04 +0700

    Update main.cpp
    ve khung

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
-#include <iostream>
+#include <stdio.h>
+#include <conio.h>  
+
+#define WIDTH 20   
+#define HEIGHT 10  
+
+// Hàm tạo khung
+void createFrame() {
+    int x, y;
+    for (y = 0; y <= HEIGHT; y++) {
+        for (x = 0; x <= WIDTH; x++) {
+            if (x == 0 || x == WIDTH) {
+                printf("#");
+            }
+            else if (y == 0 || y == HEIGHT) {
+                printf("#");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+  
+    createFrame();
+
+   
+    getch();
+
+    return 0;
+}
