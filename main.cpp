ac8065d143a78d32d19e97c95ec2fdb82a8c64f1
Sang <sang.dyna.solution@gmail.com>
2024-09-10 18:06:51 +0700

    Code an moi

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+// Khởi tạo vị trí của thức ăn (fruit):
+fruitX = rand() % width;
+fruitY = rand() % height;
+if (x == fruitX && y == fruitY) {
+    score += 10; // Tăng điểm khi ăn thức ăn
+    fruitX = rand() % width; // Tạo vị trí mới cho thức ăn
+    fruitY = rand() % height;
+    nTail++; // Tăng chiều dài của con rắn
+}
 int main() {
     float a,b,delta,x1,x2;
 
