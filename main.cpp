1dc35ff2520a1104fba2e339d565596be24960f1
UIT.23730220 <23730220@ms.uit.edu.vn>
2024-09-11 12:15:24 +0700

    Minor change Tinh-Diem

--- main.cpp
+++ main.cpp
@@ -2,14 +2,9 @@
 
 using namespace std;
 
+void Score();
+
 int main() {
-    float a,b,delta,x1,x2;
-
-    cout << "Nhap vao a: ";
-    cin >> a;
-    cout << "Nhap vao b: ";
-    cin >> b;
-
 
     return 0;
 }
