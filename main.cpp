ccc755bc80a6763c8965f4ef74bb139de8561dba
UIT.23730220 <23730220@ms.uit.edu.vn>
2024-09-11 12:57:23 +0700

    Rearrange main source

--- main.cpp
+++ main.cpp
@@ -1,13 +1,60 @@
-
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
 #include <stdio.h> 
 
+// Define game dimensions
+#define WIDTH 20
+#define HEIGHT 20
 
-#define WIDTH 20   
-#define HEIGHT 20  
+// Direction enumerator for movement
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+Direction dir;
 
+// Game state variables
+bool gameOver;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+
+void Input();
+void Logic();
+void createFrame();
+void menuGame();
+void Score(int& thoat, char name[], int& lever, int score);
+void playgame(int& thoat, char name[], int& lever);
+
+int main() {
+  
+    createFrame();
+
+   
+    getch();
+
+
+    // Kiểm tra va chạm với đuôi
+    for (int i = 0; i < nTail; i++) {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+}
+
+void menuGame(){
+    cout << "Chon chuc nang duoc hien thi ben duoi:\n";
+    cout << "1. Play!\n";
+    cout << "2. Exit.\n";
+    int choice;
+    cin >> choice;
+    switch (choice){
+        case 1: 
+            break;
+        case 2:
+            break;
+        default:
+            cout << "invalid choice.\n" << endl;
+            break;
+    }
+}
 
 void Input() {
     if (_kbhit()) {
@@ -30,6 +77,7 @@ void Input() {
         }
     }
 }
+
 void Logic() {
     int prevX = tailX[0];
     int prevY = tailY[0];
@@ -64,12 +112,10 @@ void Logic() {
     default:
         break;
     }
-
-    // Giới hạn vị trí rắn
     if (x >= width) x = 0; else if (x < 0) x = width - 1;
     if (y >= height) y = 0; else if (y < 0) y = height - 1;
+}
 
-// Hàm tạo khung
 void createFrame() {
     int x, y;
     for (y = 0; y <= HEIGHT; y++) {
@@ -86,67 +132,4 @@ void createFrame() {
         }
         printf("\n");
     }
-}
-
-void menuGame();
-void Score();
-
-int main() {
-  
-    createFrame();
-
-   
-    getch();
-
-
-    // Kiểm tra va chạm với đuôi
-    for (int i = 0; i < nTail; i++) {
-        if (tailX[i] == x && tailY[i] == y)
-            gameOver = true;
-    }
-}
-
-void menuGame(){
-    cout << "Chon chuc nang duoc hien thi ben duoi:\n";
-    cout << "1. Play!\n";
-    cout << "2. Exit.\n";
-    int choice;
-    cin >> choice;
-    switch (choice){
-        case 1: 
-            break;
-        case 2:
-            break;
-        default:
-            cout << "invalid choice.\n" << endl;
-            break;
-    }
-}
-void Score(int &thoat, char name[], int &lever, Snake &luusnake, char dname[])
-{
-  khungplay();
-  gotoxy(34, 0); TextColor(160); printf(" HIGHT SCORE ");
-  TextColor(0);
-        for(int i = 0; i<45; i++)
-    {
-        gotoxy(18+i, 11); printf(" ");
-    }
-        for(int i = 0; i<45; i++)
-    {
-        gotoxy(18+i, 21); printf(" ");
-    }
- gotoxy(33, 6); TextColor(12); printf("DIEM CAO NHAT");  
- gotoxy(25, 8); TextColor(11); printf("Ten Nguoi Choi:");       gotoxy(41, 8); TextColor(15); printf("%s", dname);
- gotoxy(25, 9); TextColor(11); printf("Diem Cao:");             gotoxy(41, 9); TextColor(10); printf("%d", luusnake.n);
-    gotoxy(36,36); TextColor(47); printf("  EXIT  ");
-    gotoxy(39,36);
-    TextColor(0);
- char a;
-    do
-    {
-        a = getch();
-    }while(a!=13 && a != 'e' && a != 'E' && a !='5' && a != 8);
-    system("cls");
-    fflush(stdin);
-    playgame(thoat, name, lever, luusnake, dname);  
 }
