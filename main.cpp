0812675d77758d85c7815619fe379741fd4347f8
UIT-23730212 <23730212@ms.uit.edu.vn>
2024-09-11 18:58:17 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -67,59 +67,7 @@ void Input() { // TRẦN NHƯ PHONG
     }
 }
 
-void Logic() {//NGUYỄN ĐĂNG SANG
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x; // Store snake head position
-    tailY[0] = y;
 
-    // Update snake tail positions
-    for (int i = 1; i < nTail; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-
-    // Update snake head position based on direction
-    switch (dir) {
-    case LEFT:
-        x--;
-        break;
-    case RIGHT:
-        x++;
-        break;
-    case UP:
-        y--;
-        break;
-    case DOWN:
-        y++;
-        break;
-    default:
-        break;
-    }
-
-    // Wrap around screen edges
-    if (x >= WIDTH) x = 0; else if (x < 0) x = WIDTH - 1;
-    if (y >= HEIGHT) y = 0; else if (y < 0) y = HEIGHT - 1;
-
-    // Check for collision with the tail
-    for (int i = 0; i < nTail; i++) {
-        if (tailX[i] == x && tailY[i] == y)
-            gameOver = true;
-    }
-
-    // Check if snake eats the fruit
-    if (x == fruitX && y == fruitY) {
-        score += 10;
-        fruitX = rand() % WIDTH;
-        fruitY = rand() % HEIGHT;
-        nTail++;
-    }
-}
 
 void createFrame() { // PHAN NHẬT HÒA
     int x, y;
